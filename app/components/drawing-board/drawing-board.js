e752aebcb4ce7b7b19f6d642b384471c7559e6c9
Pan K <paninee@khantidhara.com>
2017-05-03 09:35:53 -0700

    Add points to collection on click

--- app/components/drawing-board/drawing-board.js
+++ app/components/drawing-board/drawing-board.js
@@ -5,10 +5,20 @@ paddocksApp.directive('drawingBoard', function(){
     controller: ['$rootScope', '$scope', function ($rootScope, $scope) {
       $scope.currentX = 0;
       $scope.currentY = 0;
+      $scope.points = [];
+
       $scope.updateCurrentLocation = function($event){
         $scope.currentX = $event.layerX;
         $scope.currentY = $event.layerY;
-      }
+      };
+
+      $scope.addPoint = function($event){
+        $scope.points.push({
+          x: $event.layerX,
+          y: $event.layerY
+        });
+        console.log($scope.points);
+      };
     }]
   }
 });
