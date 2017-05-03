39cac11d039a0b124753b1fe2660cecb45f50563
Pan K <paninee@khantidhara.com>
2017-05-03 12:13:07 -0700

    Save and display collection

--- app/collection/collection.js
+++ app/collection/collection.js
@@ -1,6 +1,19 @@
 'use strict';
 
 paddocksApp.controller('CollectionCtrl', ['$rootScope', '$scope', function($rootScope, $scope) {
-  $scope.collection = JSON.parse(localStorage.getItem('paddockCollection'));
-  console.log($scope.collection);
+  var sanitizeCollection = function(collection){
+    var collectionData = [];
+    for(var i = 0; i < collection.length; i++){
+      var points = '';
+      for(var j = 0; j < collection[i].length; j++){
+        console.log(collection[i][j]);
+        points += collection[i][j].x + ',' + collection[i][j].y + ' ';
+      }
+      collectionData.push({
+        points: points
+      });
+    }
+    return collectionData;
+  };
+  $scope.collection = (localStorage.getItem('paddockCollection') != null) ? sanitizeCollection(JSON.parse(localStorage.getItem('paddockCollection'))) : [];
 }]);
